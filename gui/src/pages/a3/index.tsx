2784e11bf400b4a6f7db66de9a856d560faf2d2c
csxi <csxi@amarsoft.com>
2025-03-26 11:28:06 +0800

    需要更新依赖，执行install-dependencies

--- gui/src/pages/a3/index.tsx
+++ gui/src/pages/a3/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import PageHeader from "../../components/PageHeader";
+import { PageHeader } from "../../components/PageHeader";
 import styles from './index.module.css';
 
 interface ServiceInfo {
